15230094953bd5f572ab43d53dea2ffd9b77845d
Francisco Figueroa <ffigueroa@frro.utn.edu.ar>
2026-05-08 20:35:52 -0300

    Align book list endpoint with API

--- src/main/java/com/utn/chapterone/controllers/LibroController.java
+++ src/main/java/com/utn/chapterone/controllers/LibroController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/libros")
+@RequestMapping("/api/libros")
+@CrossOrigin(origins = "http://localhost:5173")
 public class LibroController {
 
     @Autowired
