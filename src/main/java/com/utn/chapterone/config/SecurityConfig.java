d4fd13461193710d31fcd77c67f6e3a6369dcfd1
NachoTaborda <itaborda@frro.utn.edu.ar>
2026-06-30 15:46:25 -0300

    Corrijo error 403 al hacer GET

--- src/main/java/com/utn/chapterone/config/SecurityConfig.java
+++ src/main/java/com/utn/chapterone/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class SecurityConfig {
@@ -26,6 +27,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/images/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/autores/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/clubes/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/generos/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/libros/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
@@ -34,7 +39,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
